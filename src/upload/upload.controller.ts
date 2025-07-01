3c611ba44b2acf15fc3ab9aabe83736490c16817
Ashutosh Kumar <itsray650@gmail.com>
2025-07-01 18:26:23 +0530

    put

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -11,63 +11,63 @@ export class UploadController {
         private uploadService: UploadService
     ) { }
 
-
-
-    //1. Upload on lOcal server
     @Post()
-    @UseInterceptors(FileInterceptor('file', {  //file that we are going to upload, it uses mullter middleware
+    @UseInterceptors(FileInterceptor('file', {
         storage: diskStorage({
             destination: './files',
             filename: (req, file, callback) => {
                 const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-                callback(null, `${file.fieldname}-${uniqueSuffix}-${extname(file.originalname)}`)
-            }
+                callback(null, `${file.fieldname}-${uniqueSuffix}-${extname(file.originalname)}`) }
         })
     }))
     async uploadFile(@UploadedFile(new ParseFilePipe({
         fileIsRequired: true,
-        validators: [new MaxFileSizeValidator({ maxSize: 1000000 }),]  //new FileTypeValidator({fileType : 'image/png'})
+        validators: [new MaxFileSizeValidator({ maxSize: 1000000 }),]
     })) file: Express.Multer.File) {
-        const size = Number(file.size)*1024;
-        if (size>10) {
-            await this.uploadService.SaveMetaDate(file, file.path);
-        }
-        else {
-            try {
-                const result = await this.cloud.uploadFileFromPath(file.path);
-                await fs.unlink(file.path);
-                return await this.uploadService.SaveMetaDate(file, result)
-            }
-            catch (error) {
-                throw new Error("Failed to upload image")
-            }
-        }
-        return { msg: "File Uploaded" }
+        return this.uploadService.uploadFile(file);
     }
 
 
-    // Direct Cloud
+
     @Post('cloud')
     @UseInterceptors(FileInterceptor('file', {
         storage: memoryStorage()
     }))
     async UploadFileDirectToCloud(@UploadedFile() file: Express.Multer.File) {
-        try {
-            const url = await this.cloud.uploadFile(file);
-            return await this.uploadService.SaveMetaDate(file, url);
-        }
-        catch (error) {
-            throw new InternalServerErrorException('Failed to upload image');
-        }
+        return this.uploadService.UploadFileDirectToCloud(file)
     }
 
 }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //2.SERVER TO CLOUD
 // @Post('severtocloud')
-// @UseInterceptors(FileInterceptor('file', {
+// @UseInterceptors(FileInterceptor('file', { //file that we are going to upload, it uses mullter middleware
 //     storage: diskStorage({
 //         destination: './files',
 //         filename: (req, file, callback) => {
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FileTable } from './entities/file.entity';
 import { Repository } from 'typeorm';
-
+import { CloudnaryService } from 'src/cloudnary/cloudnary.service';
+import { promises as fs } from 'fs';
 @Injectable()
 export class UploadService {
-    constructor(@InjectRepository(FileTable) private repo: Repository<FileTable>) { }
+    constructor(@InjectRepository(FileTable) private repo: Repository<FileTable>,
+        private cloud: CloudnaryService,) { }
+
+
     async SaveMetaDate(file: Express.Multer.File, url: string) {
         const size = (Number)(file.size)
         const kb = Math.round(size / 1024);
@@ -17,5 +21,33 @@ export class UploadService {
         }
         await this.repo.save(data)
         return url;
+    }
+    async uploadFile(file: Express.Multer.File) {
+        const size = Number(file.size) * 1024;
+        if (size > 10) {
+            await this.SaveMetaDate(file, file.path);
+        }
+        else {
+            try {
+                const result = await this.cloud.uploadFileFromPath(file.path);
+                await fs.unlink(file.path);
+                return await this.SaveMetaDate(file, result)
+            }
+            catch (error) {
+                throw new Error("Failed to upload image")
+            }
+        }
+        return { msg: "File Uploaded" }
+    }
+
+
+    async UploadFileDirectToCloud(file: Express.Multer.File) {
+        try {
+            const url = await this.cloud.uploadFile(file);
+            return await this.SaveMetaDate(file, url);
+        }
+        catch (error) {
+            throw new InternalServerErrorException('Failed to upload image');
+        }
     }
 }
